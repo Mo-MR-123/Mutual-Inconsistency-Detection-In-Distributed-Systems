b328f984874f72c0f3f7296ce5c7e1ac306fc739
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-02-21 22:11:42 +0100

    uncommented test

--- src/test/scala/com/akkamidd/AkkaMainSpec.scala
+++ src/test/scala/com/akkamidd/AkkaMainSpec.scala
@@ -2,17 +2,17 @@
 package com.akkamidd
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-//import com.akkamidd.Greeter.Greet
-//import com.akkamidd.Greeter.Greeted
+import com.akkamidd.Greeter.Greet
+import com.akkamidd.Greeter.Greeted
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class AkkaMainSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   "A Greeter" must {
     "reply to greeted" in {
-//      val replyProbe = createTestProbe[Greeted]()
-//      val underTest = spawn(Greeter())
-//      underTest ! Greet("Santa", replyProbe.ref)
-//      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
+      val replyProbe = createTestProbe[Greeted]()
+      val underTest = spawn(Greeter())
+      underTest ! Greet("Santa", replyProbe.ref)
+      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
     }
   }
 }
