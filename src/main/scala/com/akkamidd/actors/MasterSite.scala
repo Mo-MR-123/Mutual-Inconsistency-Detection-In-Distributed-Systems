d08686c518ab97ccb8981d4847646d79e26267e7
hanzhanglin <linhanzhang_07@outlook.com>
2022-03-15 12:30:57 +0100

    add findPartitionSet

--- src/main/scala/com/akkamidd/actors/MasterSite.scala
+++ src/main/scala/com/akkamidd/actors/MasterSite.scala
@@ -3,12 +3,23 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import com.akkamidd.actors.Site.SiteProtocol
 
+import scala.reflect.Manifest.Nothing
 import scala.util.hashing.MurmurHash3
 
 // the master actor who spawn the sites
 object MasterSite {
   sealed trait MasterSiteProtocol
   final case class Broadcast(msg: Site.SiteProtocol, from: ActorRef[Site.SiteProtocol]) extends MasterSiteProtocol
+
+  def findPartitionSet(from: ActorRef[SiteProtocol],sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]]): Set[ActorRef[SiteProtocol]] = {
+    for (set <- sitesPartitionedList) {
+      if (set.contains(from)) {
+        return set
+      }
+
+    }
+    Set[ActorRef[SiteProtocol]]()
+  }
 
   def apply(): Behavior[MasterSiteProtocol] =
     Behaviors.setup { context =>
@@ -17,8 +28,11 @@ object MasterSite {
       val siteA = context.spawn(Site(), "A")
       val siteB = context.spawn(Site(), "B")
       val siteC = context.spawn(Site(), "C")
+      val siteD = context.spawn(Site(), "D")
 
-      val sitesList: List[ActorRef[SiteProtocol]] = List(siteA, siteB, siteC)
+      // val sitesList: List[ActorRef[SiteProtocol]] = List(siteA, siteB, siteC, siteD)
+      val init_sitePartitionList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB,siteC,siteD))
+      val sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB),Set(siteC,siteD))
 
       // upload files
       val time_a1 = System.currentTimeMillis().toString
@@ -29,18 +43,24 @@ object MasterSite {
       // update files
       siteA ! Site.FileUpdate(("A", time_a1), context.self)
       siteA ! Site.FileUpdate(("A", time_a1), context.self)
+      siteB ! Site.FileUpdate(("A", time_a1), context.self)
+      siteC ! Site.FileUpdate(("A", time_a1), context.self)
 
-//      //siteA ! Site.printMap()
-//
-//      context.system.scheduler.scheduleOnce(Duration(2, TimeUnit.SECONDS),siteA,Site.printMap() )
-////      context.system.scheduler.scheduleOnce(Duration(2, TimeUnit.SECONDS ) {
-////        siteA ! Site.printMap()
-////      }
+      //      //siteA ! Site.printMap()
+      //
+      //      context.system.scheduler.scheduleOnce(Duration(2, TimeUnit.SECONDS),siteA,Site.printMap() )
+      ////      context.system.scheduler.scheduleOnce(Duration(2, TimeUnit.SECONDS ) {
+      ////        siteA ! Site.printMap()
+      ////      }
 
       Behaviors.receiveMessage {
         case MasterSite.Broadcast(msg: SiteProtocol, from: ActorRef[SiteProtocol]) =>
-          sitesList.foreach { child =>
-            child ! msg
+          val partitionSet = findPartitionSet(from: ActorRef[SiteProtocol], sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]] )
+          partitionSet.foreach { child =>
+            if(!child.equals(from)) {
+              child ! msg
+              context.log.info("from {} , send message to {}",from,child.toString)
+            }
           }
           Behaviors.same
 
