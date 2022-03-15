4a512da70d74fdbdf53c218caff0d8d695022528
hanzhanglin <linhanzhang_07@outlook.com>
2022-03-15 14:21:54 +0100

    add split method and test

--- src/main/scala/com/akkamidd/actors/MasterSite.scala
+++ src/main/scala/com/akkamidd/actors/MasterSite.scala
@@ -11,6 +11,25 @@ object MasterSite {
   sealed trait MasterSiteProtocol
   final case class Broadcast(msg: Site.SiteProtocol, from: ActorRef[Site.SiteProtocol]) extends MasterSiteProtocol
 
+  //find the partition that the part is in
+  def splitPartition(sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]],partToSplit:Set[ActorRef[SiteProtocol]]): List[Set[ActorRef[SiteProtocol]]] ={
+    var newPartitionList:List[Set[ActorRef[SiteProtocol]]] = sitesPartitionedList
+    for (set <- newPartitionList){
+      if (partToSplit.subsetOf(set)) {
+        // remove The old partition
+        newPartitionList = newPartitionList.filter(!_.equals(set))
+        // create new partition for the remaining part
+        val setRemain = set -- partToSplit
+        newPartitionList = newPartitionList :+ setRemain
+        // create new partition for the partToSplit and append the new one to partition list
+        newPartitionList = newPartitionList :+ partToSplit
+        return newPartitionList
+      }
+    }
+    throw new Exception("Not valid sub-partition in current DAG")
+  }
+
+  // given a site "from", find a partition that the site is currently in
   def findPartitionSet(from: ActorRef[SiteProtocol],sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]]): Set[ActorRef[SiteProtocol]] = {
     for (set <- sitesPartitionedList) {
       if (set.contains(from)) {
@@ -18,6 +37,7 @@ object MasterSite {
       }
 
     }
+    // if the site is not found in partitionList , return a empty set
     Set[ActorRef[SiteProtocol]]()
   }
 
@@ -30,9 +50,18 @@ object MasterSite {
       val siteC = context.spawn(Site(), "C")
       val siteD = context.spawn(Site(), "D")
 
+      // test for spliting
       // val sitesList: List[ActorRef[SiteProtocol]] = List(siteA, siteB, siteC, siteD)
-      val init_sitePartitionList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB,siteC,siteD))
-      val sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB),Set(siteC,siteD))
+      var init_sitePartitionList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB,siteC,siteD))
+      // split into {A,B}{C,D}
+      init_sitePartitionList = splitPartition(init_sitePartitionList,Set(siteA))
+      context.log.info("Split 1, new PartitionList: {}",init_sitePartitionList)
+
+      init_sitePartitionList = splitPartition(init_sitePartitionList,Set(siteB,siteC))
+      context.log.info("Split 2, new PartitionList: {}",init_sitePartitionList)
+
+
+      var sitesPartitionedList: List[Set[ActorRef[SiteProtocol]]] = List(Set(siteA,siteB),Set(siteC,siteD))
 
       // upload files
       val time_a1 = System.currentTimeMillis().toString
