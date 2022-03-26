baf9dd1ad9011ae80abb2f8c2884d3a2a219c63a
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-03-26 16:35:32 +0100

    some progress with arg parsing

--- src/main/scala/com/akkamidd/AkkaMain.scala
+++ src/main/scala/com/akkamidd/AkkaMain.scala
@@ -5,20 +5,95 @@ import com.akkamidd.actors.MasterSite
 import com.akkamidd.actors.MasterSite.{FileUpdateMasterSite, FileUploadMasterSite, MasterSiteProtocol, Merge, SpawnSite}
 import org.slf4j.Logger
 
+
+object AkkaMain extends App {
+  println(args.length)
+  println(args.mkString("Array(", ", ", ")"))
+
+  if (args.length == 1) { // run experiments
+    args(0) match {
+      case "0" =>
+        Experiments.experiment0(1000, 500, 500)
+    }
+  } else {
+    val numSiteActors = args(0).toLong
+
+//    if (numSiteActors < 2) {
+//      throw new Exception("It is not possible to run less than 2 Site Actors. Please provide 2 or more Site Actors.")
+//    }
+
+    val masterSystem = ActorSystem(MasterSite(), "MasterSite")
+    val partitionList = UtilFuncs.spawnSites(masterSystem, List.range(0, numSiteActors).map(_.toString), 2000)
+
+    args.tail.foreach { // run
+      case command if command.contains("upload") =>
+        val splitCommand = command.split("-").tail
+        val currTimestamp = System.currentTimeMillis().toString
+        val siteToUpload = splitCommand(0)
+        val fileName = splitCommand(1)
+        masterSystem ! FileUploadMasterSite(siteToUpload, currTimestamp, fileName, partitionList)
+
+
+    }
+
+    masterSystem.terminate()
+  }
+
+}
+
+object Experiments {
+  def experiment0(
+                   spawningActorsTimeout: Long,
+                   timeoutSplit: Long,
+                   timeoutMerge: Long
+                 ): Unit =
+  {
+    val experimentStartMillis = System.currentTimeMillis()
+
+    val masterSite: ActorSystem[MasterSiteProtocol] = ActorSystem(MasterSite(), "MasterSite")
+
+    var partitionList: List[Set[String]] = UtilFuncs.spawnSites(masterSite, List("A", "B", "C", "D"), spawningActorsTimeout)
+
+    // upload files
+    val time_a1 = System.currentTimeMillis().toString
+    masterSite ! FileUploadMasterSite("A", time_a1, "test.txt", partitionList)
+
+    // split into {A,B} {C,D}
+    partitionList = UtilFuncs.callSplit(masterSite, partitionList, Set("A", "B"), timeoutSplit, timeoutSplit)
+
+    masterSite ! FileUpdateMasterSite("A", ("A", time_a1), partitionList)
+    masterSite ! FileUpdateMasterSite("B", ("A", time_a1), partitionList)
+
+    masterSite ! FileUpdateMasterSite("C", ("A", time_a1), partitionList)
+
+    //  merge into {A, B, C, D}
+    partitionList = UtilFuncs.callMerge("A", "C", masterSite, partitionList, Set("A", "B", "C", "D"), timeoutMerge, timeoutMerge)
+
+    val experimentEndMillis = System.currentTimeMillis() - timeoutMerge*2 - timeoutSplit*2 - spawningActorsTimeout
+
+    masterSite.terminate()
+
+    println(s"[Experiment 0] Execution time in millis: ${experimentEndMillis - experimentStartMillis}")
+  }
+}
+
+
 object UtilFuncs {
   /**
    * Send messages to MasterSite to instruct it to create a new Site Actor in the Actor System.
-   * @param masterSystem
-   * @param siteNameList
-   * @param partitionList
-   * @param timeout
-   * @return
+   * @param masterSystem The Actor System that is used to send messages to the MasterSite
+   * @param siteNameList The names of the Site Actors to spawn
+   * @param timeout The incurred timeout after sending the Spawn Messages to the master sites to
+   *                ensure all sites are spawned before continuing with other operations on the system
+   * @param partitionList The initial partition list to use, List() by default
+   * @return The partition list that contains 1 set containing the names of the spawned sites.
+   *         We assume that all Site Actors are initially in 1 partition.
    */
   def spawnSites(
                   masterSystem: ActorSystem[MasterSiteProtocol],
                   siteNameList: List[String],
-                  partitionList: List[Set[String]],
-                  timeout: Long
+                  timeout: Long,
+                  partitionList: List[Set[String]] = List(),
                 ): List[Set[String]] =
   {
     siteNameList.foreach(siteName => {
@@ -146,27 +221,4 @@ object UtilFuncs {
     result.deleteCharAt(result.length()  - 1)
     logger.info(result.toString())
   }
-}
-
-object AkkaMain extends App {
-
-  val masterSite: ActorSystem[MasterSiteProtocol] = ActorSystem(MasterSite(), "MasterSite")
-
-  var partitionList: List[Set[String]] = UtilFuncs.spawnSites(masterSite, List("A", "B", "C", "D"), List(), 1000)
-
-  // upload files
-  val time_a1 = System.currentTimeMillis().toString
-  masterSite ! FileUploadMasterSite("A", time_a1, "test.txt", partitionList)
-
-  // split into {A,B} {C,D}
-  partitionList = UtilFuncs.callSplit(masterSite, partitionList, Set("A", "B"), 500, 500)
-
-  masterSite ! FileUpdateMasterSite("A", ("A", time_a1), partitionList)
-  masterSite ! FileUpdateMasterSite("B", ("A", time_a1), partitionList)
-
-  masterSite ! FileUpdateMasterSite("C", ("A", time_a1), partitionList)
-
-  //  merge into {A, B, C, D}
-  partitionList = UtilFuncs.callMerge("A", "C", masterSite, partitionList, Set("A", "B", "C", "D"), 500, 500)
-
 }
