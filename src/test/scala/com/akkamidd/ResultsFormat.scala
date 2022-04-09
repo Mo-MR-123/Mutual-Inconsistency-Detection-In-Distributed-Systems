77c9ad0189a873d644fbcfcc3df70d26d9b941c4
Adam <adam_school@outlook.com>
2022-04-09 16:09:02 +0200

    Working results aggregation

--- src/test/scala/com/akkamidd/ResultsFormat.scala
+++ src/test/scala/com/akkamidd/ResultsFormat.scala
@@ -3,14 +3,37 @@ import scala.io.Source
 
 object ResultsFormat {
 
-  def readFile(filename: String) = {
-    val icdCount = Source.fromFile(filename).getLines.map{ _.toInt
+  def intCount(filename: String): Int = {
+    val count = Source.fromFile(filename).getLines.map{ _.toInt
     }.foldLeft(0)(_+_)
-    print(icdCount)
+    count
   }
 
   def main(args: Array[String]) {
-    readFile("output/run1_experiment1_icd.txt")
+    val numberOfExperiments = 1
+    val numberOfRuns = 10
+
+
+
+    for (j <- 1 to numberOfExperiments) {
+      print("Experiment" + j + ":\n")
+      var icdSum: Int = 0
+      var execSum: Int = 0
+
+      for (i <- 1 to numberOfRuns) {
+        val icdCount = intCount("output/run" + i + "_experiment" + j + "_icd.txt")
+        val execCount = intCount("output/run" + i + "_experiment" + j + "_exec.txt")
+        icdSum += icdCount
+        execSum += execCount
+        print(s"Run ${i} - Execution time: ${execCount}, icd count: ${icdCount} \n")
+      }
+
+      val icdAverage = icdSum / numberOfRuns
+      val execAverage = execSum / numberOfRuns
+      print(s"Experiment ${j} - Average execution time: ${execAverage}, icd average count: ${icdAverage} \n")
+    }
+
+
 
 //    val exec = Source.fromFile("output/run1_experiment1_exec.txt").mkString
 //    val icd = Source.fromFile("output/run1_experiment1_icd.txt")
